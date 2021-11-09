f0ed27a412a3a64c7837629e5c50b31d829c63ec
rogowskm <maksymilian.rogowski@tomtom.com>
2021-11-09 13:11:03 +0100

    Change templates for global

--- pywww/settings.py
+++ pywww/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'pywww.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR)), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
